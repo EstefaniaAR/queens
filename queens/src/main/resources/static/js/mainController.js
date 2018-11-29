e9bf5909753e2573f26fb44f671f37ef2f683966
Estefani <estefania@outlook.com>
2018-11-29 16:44:44 -0600

    business logic almost done, it lefts sorting the arrays and show them. Also fix the view

--- queens/src/main/resources/static/js/mainController.js
+++ queens/src/main/resources/static/js/mainController.js
@@ -1,6 +1,6 @@
 var app = angular.module("app", []);
 
-app.controller("mainController", ["$scope","$http",function($scope, $http) 
+app.controller("mainController", ["$scope","$http","$q",function($scope, $http,$q) 
 {
 	$scope.heartPile=[];
 	$scope.diamondPile=[];
@@ -8,26 +8,48 @@ app.controller("mainController", ["$scope","$http",function($scope, $http)
 	$scope.spadePile=[];
 	$scope.queenPile = [];
 	$scope.log="".trim();
+
+	$scope.doStuff= function()
+	{
+		var promise =suffle();
+		promise.then(function(greeting) 
+		{
+			$scope.deck= promise.$$state.value;
+			draw();
+		}, function(reason) {
+		  alert('Failed: ' + reason);
+		});
+	}
 	
-    $scope.suffleDeck = function() 
-    {
-    	var resource = "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1";
-        $http.get(resource).then(
-        function successCallback(response) 
-        {
-          $scope.deck = response.data;
-        },
-        function errorCallback(response) 
-        {
-          alert("Error:" + JSON.stringify(response.data));
-          console.log(response);
-        }
-      );
-    };
-    
-    $scope.drawCard = function() 
-    {
+	function suffle()
+	{
+		return $q (function (resolve,reject)
+		{
+			console.log("Suffling...");
+	    	var resource = "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1";
+	    	setTimeout( function ()
+	    	{
+		        $http.get(resource).then(
+			        function successCallback(response) 
+			        {
+			          resolve (response.data);          
+			        },
+			        function errorCallback(response) 
+			        {
+			          alert("Error:" + JSON.stringify(response.data));
+			          reject(JSON.stringify(response.data));
+			        }
+		      	);
+	    	},1000);
+		});
+	};
+	
+	function draw()
+	{
+		console.log("drwaing card...");
+    	console.log($scope.deck)
     	var resource = "https://deckofcardsapi.com/api/deck/"+$scope.deck.deck_id+"/draw/?count=1";
+    	
         $http.get(resource).then(
         function successCallback(response) 
         {
@@ -56,7 +78,121 @@ app.controller("mainController", ["$scope","$http",function($scope, $http)
         	  $scope.queenPile.push($scope.deck.cards[0]);
           }
           $scope.log = $scope.log+ "Machine draws a "+ $scope.deck.cards[0].value +" of "+ $scope.deck.cards[0].suit + " card.\r\n";
+          if($scope.queenPile.length < 4)
+        	  draw();
           
+        },
+        function errorCallback(response) 
+        {
+          alert("Error:" + JSON.stringify(response.data));
+          console.log(response);
+        }
+      );
+	}
+	function draws()
+	{
+		return $q (function (resolve,reject)
+		{
+			console.log("Suffling...");
+	    	var resource = "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1";
+	    	setTimeout( function ()
+	    	{
+	    		console.log("drwaing card...");
+	        	console.log($scope.deck)
+	        	var resource = "https://deckofcardsapi.com/api/deck/"+$scope.deck.deck_id+"/draw/?count=1";
+	        	
+	            $http.get(resource).then(
+	            function successCallback(response) 
+	            {
+	              $scope.deck = response.data;
+	              switch ($scope.deck.cards[0].suit)
+	              {
+	              	case "HEARTS":
+	              		$scope.heartPile.push($scope.deck.cards[0]);
+	              		break;
+	              		
+	              	case "DIAMONDS":
+	              		$scope.diamondPile.push($scope.deck.cards[0]);
+	              		break;
+	              		
+	              	case "CLUBS":
+	              		$scope.clubPile.push($scope.deck.cards[0]);
+	              		break;
+	              		
+	              	case "SPADES":
+	              		$scope.spadePile.push($scope.deck.cards[0]);
+	              		break;
+	              }
+	              
+	              if($scope.deck.cards[0].value == "QUEEN")
+	              {
+	            	  $scope.queenPile.push($scope.deck.cards[0]);
+	              }
+	              $scope.log = $scope.log+ "Machine draws a "+ $scope.deck.cards[0].value +" of "+ $scope.deck.cards[0].suit + " card.\r\n";
+	              
+	              
+	            },
+	            function errorCallback(response) 
+	            {
+	              alert("Error:" + JSON.stringify(response.data));
+	              console.log(response);
+	            }
+	          );
+	    	},1000);
+		});
+	};
+	
+	function base()
+	{
+		return $q (function (resolve,reject)
+		{
+			console.log("Suffling...");
+	    	var resource = "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1";
+	    	setTimeout( function ()
+	    	{
+		        
+	    	},1000);
+		});
+	};
+	
+
+
+    $scope.drawCard = function() 
+    {
+    	console.log("drwaing card...");
+    	console.log($scope.deck)
+    	var resource = "https://deckofcardsapi.com/api/deck/"+$scope.deck.deck_id+"/draw/?count=1";
+    	
+        $http.get(resource).then(
+        function successCallback(response) 
+        {
+          $scope.deck = response.data;
+          switch ($scope.deck.cards[0].suit)
+          {
+          	case "HEARTS":
+          		$scope.heartPile.push($scope.deck.cards[0]);
+          		break;
+          		
+          	case "DIAMONDS":
+          		$scope.diamondPile.push($scope.deck.cards[0]);
+          		break;
+          		
+          	case "CLUBS":
+          		$scope.clubPile.push($scope.deck.cards[0]);
+          		break;
+          		
+          	case "SPADES":
+          		$scope.spadePile.push($scope.deck.cards[0]);
+          		break;
+          }
+          
+          if($scope.deck.cards[0].value == "QUEEN")
+          {
+        	  $scope.queenPile.push($scope.deck.cards[0]);
+          }
+          $scope.log = $scope.log+ "Machine draws a "+ $scope.deck.cards[0].value +" of "+ $scope.deck.cards[0].suit + " card.\r\n";
+          if($scope.deck.queenPile.length < 4)
+        	  $scope.draw();
         },
         function errorCallback(response) 
         {
@@ -104,8 +240,8 @@ app.controller("mainController", ["$scope","$http",function($scope, $http)
         	valueA = Number(valueA);
         	valueB = Number(valueB);
         	
-        	console.log("value: "+a.value+" suit: "+a.suit+" a:"+valueA+" type:"+typeof valueA);
-        	console.log("value: "+b.value+" suit: "+b.suit+" b:"+valueB+" type: "+typeof valueB);
+        	//console.log("value: "+a.value+" suit: "+a.suit+" a:"+valueA+" type:"+typeof valueA);
+        	//console.log("value: "+b.value+" suit: "+b.suit+" b:"+valueB+" type: "+typeof valueB);
         		
         	if(valueA > valueB)
         		return 1;
@@ -119,3 +255,21 @@ app.controller("mainController", ["$scope","$http",function($scope, $http)
     
   }
 ]);
+
+
+app.factory("suffle",function ($http) 
+{
+	console.log("Suffling...");
+	var resource = "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1";
+    return $http.get(resource).then(
+	    function successCallback(response) 
+	    {
+	      return response.data;          
+	    },
+	    function errorCallback(response) 
+	    {
+	      alert("Error:" + JSON.stringify(response.data));
+	      console.log(response);
+	    }
+    );
+});
